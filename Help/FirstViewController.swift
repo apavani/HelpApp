19f770d57cb4dc1d1a1d52b2c2c4a7ef1df0b403
Adarsh <adarshkumar.pavani@gmail.com>
2014-10-04 20:46:12 -0400

    Adding code for location pushed to parse

--- Help/FirstViewController.swift
+++ Help/FirstViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager : CLLocationManager!
+    var latitude: String!
+    var longitude: String!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //var addLocation : PFObject = PFObject(className: "PeopleLocation")
+        
+        //self.locationManager = CLLocationManager()
+        //self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //self.locationManager.delegate = self
+        
+        
+        //Setting other variables in the PFObject
+        //addLocation["Latitude"] = 40.50
+        //addLocation["Longitude"] = 50.22014
+       // addLocation.saveInBackground()
+        
     }
 
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        // Most recent updates are appended to end of array,
+        // so find the most recent update in last index.
+        var loc : CLLocation = locations?[locations.count - 1] as CLLocation
+        
+        // The location stored as a coordinate.
+        var coord : CLLocationCoordinate2D = loc.coordinate
+        
+        // Set the location label to the coordinates of location.
+        latitude = "\(coord.latitude)"
+        longitude = "\(coord.longitude)"
+        
+        // Tell location manager to stop collecting and updating location.
+        self.locationManager.stopUpdatingLocation()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
