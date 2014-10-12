b5178f9a5b6f1c0afd64dcc2dad3095fc20b54a2
John Anderson <liqihuimm@gmail.com>
2014-10-12 19:54:02 -0400

    temp commit

--- Header.m
+++ Header.m
@@ -1,9 +1,9 @@
 //
-//  Help-Bridging-Header.h
+//  Header.m
 //  Help
 //
 //  Created by LiQihui on 10/4/14.
 //  Copyright (c) 2014 Adarshkumar Pavani. All rights reserved.
 //
 
-#import <Parse/Parse.h>
+#import <Foundation/Foundation.h>
